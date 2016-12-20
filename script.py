b0096c5c670a700e693133f09479b6d8bd1cdea7
Waner Miranda <waner@dcc.ufmg.br>
2016-12-20 02:28:41 -0200

    session new

--- script.py
+++ script.py
@@ -14,16 +14,17 @@ print "Login into the Server Just for show, need to be debugged."
 wsdl = 'http://sistemas.cvm.gov.br/webservices/Sistemas/SCW/CDocs/WsDownloadInfs.asmx?WSDL'
 client = zeep.Client(wsdl=wsdl)
 soap_header = client.service.Login('2293', '8700')
-soap_header['header']
+print soap_header['header']
 
 
 header = {
-    'sessaoIdHeader': {
-        'Guid': '2d572807-3eba-4fde-8a9c-8b1bfd503a43',
-        'IdSessao': 822240093L,
-        '_attr_1': None
+        'sessaoIdHeader': {
+            'Guid': '0af98a7b-8275-41ea-90eb-9873ddf2b0fb',
+            'IdSessao': 822283042L,
+            '_attr_1': None
+        }
     }
-}
+
 
 doc  = '209'
 dataDocs = '2016-12-01'
@@ -38,13 +39,15 @@ url_docs = result_docs['body']['solicAutorizDownloadArqComptcResult']
 print "Downloading and unziping", url_docs
 
 url = urlopen(url_docs)
-
 zipfile = ZipFile(StringIO(url.read()))
 lines = None
 for name in zipfile.namelist():
     lines = zipfile.open(name).readlines()
 
+
 data = xmltodict.parse(''.join(lines))
+
+
 
 client = MongoClient('localhost', 27017)
 db = client.CVM
